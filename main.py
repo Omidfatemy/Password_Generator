520bbe5f4a5c15340567f1a906283fc663251204
Omidfatemy <94586407+Omidfatemy@users.noreply.github.com>
2022-04-30 17:20:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,53 @@
-#Password Generator Project
+# Password Generator Project
 
-#creat lists of characters used in Password
+# create lists of characters used in Password
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+           'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-#Get necessary input from user
+# Get necessary input from user
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Main program which use inputs to randomly chose between characters
+# Main program which use inputs to randomly chose between characters
 password_list = []
 
 for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+    password_list.append(random.choice(letters))
 
 for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+    password_list += random.choice(symbols)
 
 for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
+    password_list += random.choice(numbers)
 
-#check the main outputs
-#print(password_list)
+# check the main outputs
+# print(password_list)
+
 random.shuffle(password_list)
-#print(password_list)
+
+# print(password_list)
 
 password = ""
 for char in password_list:
-  password += char
+    password += char
 
 print(f"Your password is: {password}")
 
 
-#This is a sample of How program works
-  #input
-  #Welcome to the PyPassword Generator!
-  #How many letters would you like in your password?
-  #2
-  #How many symbols would you like?
-  #3
-  #How many numbers would you like?
-  #2
+# This is a sample of How program works
 
-  #Your password is: 5*k5*I)
+# input
+# Welcome to the PyPassword Generator!
+# How many letters would you like in your password?
+# 2
+# How many symbols would you like?
+# 3
+# How many numbers would you like?
+# 2
+# Your password is: 5*k5*I)
